72f4a52b139ac36cf2b54ae888511c9937aa99d7
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 19:14:38 +0700

    menangani kondisi ketika if tidak terpenuhi atau aksi yang diharapkan gagal

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -115,5 +115,7 @@ namespace CRUDSederhana
                                     LoadData();
                                     ClearForm();
                                 }
-                                
+                                else
+                                {
+                                    
 
