257f49f04c4df479374b8082af1b267c0356f657
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 18:54:06 +0700

    menangkap dan menangani kesalahan

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -82,5 +82,7 @@ namespace CRUDSederhana
                         }
                     }
                 }
-                
+                catch (Exception ex)
+                {
+                    
 
