783ae65d8e57a26174c17e7c361ba87a99e58c57
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 17:59:38 +0700

    menampilkan pesan kesalahan

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -41,5 +41,10 @@ namespace CRUDSederhana
                 }
                 catch (Exception ex)
                 {
-                    
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        
 
