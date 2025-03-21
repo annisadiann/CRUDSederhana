d73e76df4c82a00017b2c528d26939a6bd743748
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 19:11:12 +0700

    menampilkan popup notifikasi kepada pengguna

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -111,5 +111,6 @@ namespace CRUDSederhana
 
                                 if (rowsAffected > 0)
                                 {
+                                    MessageBox.Show("Data berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
                                     
 
