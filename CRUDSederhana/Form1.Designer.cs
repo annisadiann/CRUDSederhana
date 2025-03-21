1a2922aeef743b1fa65a8378bb0ee14c9850113e
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 17:45:42 +0700

    mengisi datatable dengan hasil dari query SQL

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -34,5 +34,6 @@ namespace CRUDSederhana
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
                     DataTable dt = new DataTable();
+                    da.Fill(dt);
                     
 
