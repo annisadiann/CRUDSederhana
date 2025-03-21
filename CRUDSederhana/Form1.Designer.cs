488d70ef52f1f79f0cc0975e6b0ceb4abe7e31da
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 18:44:41 +0700

    mengecek apakah perintah SQL berhasil mempengaruhi

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -70,5 +70,7 @@ namespace CRUDSederhana
                         cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text.Trim());
 
                         int rowsAffected = cmd.ExecuteNonQuery();
-                        
+                        if (rowsAffected > 0)
+                        {
+                            
 
