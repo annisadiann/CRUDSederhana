44025a17f2cce3b06d28ee94f4edf7e4fbba26bc
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 17:47:17 +0700

    DataGridView bisa menampilkan semua data otomatis

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -35,5 +35,6 @@ namespace CRUDSederhana
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
                     DataTable dt = new DataTable();
                     da.Fill(dt);
+                    dgvMahasiswa.AutoGenerateColumns = true;
                     
 
