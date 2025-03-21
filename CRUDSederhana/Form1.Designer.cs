fcab090aab28bd49f8daf7f6329fd3186268c795
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 18:41:32 +0700

    mengisi parameter SQL @Telepon

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -66,5 +66,6 @@ namespace CRUDSederhana
                         cmd.Parameters.AddWithValue("@NIM", txtNIM.Text.Trim());
                         cmd.Parameters.AddWithValue("@Nama", txtNama.Text.Trim());
                         cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
-                        
+                        cmd.Parameters.AddWithValue("@Telepon", txtTelepon.Text.Trim());
+                       
 
