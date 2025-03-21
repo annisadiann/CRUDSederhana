46a8525f99f96a29b35ac2811c0c36e755bd97bd
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 19:09:04 +0700

    menjalankan perintah SQL

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -107,5 +107,7 @@ namespace CRUDSederhana
                             using (SqlCommand cmd = new SqlCommand(query, conn))
                             {
                                 cmd.Parameters.AddWithValue("@NIM", nim);
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
                                 
 
