792dbb6789b991087687cede57499a497ec83287
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2025-03-21 17:49:42 +0700

    menghubungkan DataGridView dengan datatable

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -36,5 +36,6 @@ namespace CRUDSederhana
                     DataTable dt = new DataTable();
                     da.Fill(dt);
                     dgvMahasiswa.AutoGenerateColumns = true;
+                    dgvMahasiswa.DataSource = dt;
                     
 
